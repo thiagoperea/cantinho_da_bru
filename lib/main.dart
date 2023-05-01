31a538b74c93cd12d5beddf5e8f354eacbd8516d
Thiago Perea <thiago.perea1@gmail.com>
2023-05-01 17:16:09 -0300

    fix names

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:cantinho_da_bru/presentation/screen/main/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CantinhoDaBruApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CantinhoDaBruApp extends StatelessWidget {
+  const CantinhoDaBruApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Cantinho da Bru',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFB34248)),
       ),
